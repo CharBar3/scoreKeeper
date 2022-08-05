d84d2d436c26b6bf21382bb624b02ef187919a6e
Charles Bartlett <ownageunit@gmail.com>
2022-08-05 01:55:23 -0400

    wip: styling, update button logic

--- src/components/PlayerCards.js
+++ src/components/PlayerCards.js
@@ -2,14 +2,19 @@
 
 const PlayerCards = ({ players, updatePlayer, removeSpecificPlayer }) => {
 
+    const updateScore = (e, index) => {
+        console.log("update score pressed")
+    }
+
     let showPlayers = players.map((element, index) => {
         return (
             <div key={index} className="player-div">
-                <div>
-                    <input className="player-title-input" type="text" name="playerName" value={element.playerName} onChange={(e) => updatePlayer(e, index)}/>   
-                </div>
+                <input className="player-title-input" type="text" name="playerName" value={element.playerName} onChange={(e) => updatePlayer(e, index)}/>   
                 <label htmlFor="playerScore">{`Current Score ${element.playerScore}`}</label>
-                <input className="player-score-input" type="number" name="playerScore" value={element.playerScore} onChange={(e) => updatePlayer(e, index)}/>
+                <div className="score-input-div">
+                    <input className="player-score-input" type="number" name="playerScore" value={element.playerScore} onChange={(e) => updatePlayer(e, index)}/>
+                    <button onClick={(e) => updateScore(e, index) }>Update</button>
+                </div>
                 <textarea className="player-notes-input" type="textarea" name="notes" value={element.notes} onChange={(e) => updatePlayer(e, index)}/>
                 <button name={index} onClick={(e) => removeSpecificPlayer(e)}>Remove Player</button>
             </div>
